4235a90bde843b4fc3d444499c685693f3ec01b9
Mitul82 <mitulsrivas@gmail.com>
2026-02-21 22:05:53 +0530

    update ai.js to newer model as mixtral has been depricated

--- src/components/ai.js
+++ src/components/ai.js
@@ -6,17 +6,19 @@ export async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
     try {
         const response = await hf.chatCompletion({
-            model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+            model: "meta-llama/Llama-3.1-8B-Instruct",
             messages: [
-                { role: "system", content: import.meta.env.VITE_AI_PROMT },
+                { role: "system", content: import.meta.env.VITE_AI_PROMPT },
                 { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
             ],
             max_tokens: 1024,
+            temperature: 0.7
         })
         console.log("Recepie received and sent res");
         return response.choices[0].message.content
     } catch (err) {
         console.error(err.message)
+        return "I'm sorry, I'm having trouble connecting to my 'chef brain' right now. Please try again in a moment!";
     }
 }
 
